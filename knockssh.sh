d62e6251a8deb0aec067dd7946a2c5141cd919ec
zeusintuivo <zeus@intuivo.com>
2023-12-24 05:34:56 +0100

    added extra line to knockssh.sh knock ssh

--- knockssh.sh
+++ knockssh.sh
@@ -3,7 +3,8 @@ echo "
 Script to port knocking
  call
      $0 __domain_or_ip_knock___    __domain_or_ip_enter__
-
+     $0  website.com                web_key_name_config_file
+		  
 REF: https://askubuntu.com/questions/867261/ssh-client-port-knocking-execute-command-before-connecting
 REF https://help.ubuntu.com/community/PortKnocking#Client_Side
 
