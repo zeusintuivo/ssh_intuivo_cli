99acc016c200f040431f81ecd2c244e18a2c7125
zeusintuivo <zeus@intuivo.com>
2024-03-28 16:21:57 +0200

    added knockscp

---
+++ knockscp.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+echo "
+Script to port knocking
+ call
+     $0 __domain_or_ip_knock___    __domain_or_ip_enter__        ___from_file_path____  __to_remove_location__
+     $0  website.com                web_key_name_config_file     /home/dev/file.py      /home/server/server/file.py
+		  
+REF: https://askubuntu.com/questions/867261/ssh-client-port-knocking-execute-command-before-connecting
+REF https://help.ubuntu.com/community/PortKnocking#Client_Side
+
+"
+if [ ${#} -lt 4 ] ;  then
+{
+  echo "error Expecting 4 params"
+  exit 1
+}
+fi
+if [[ ! -e "${3}" ]] ; then 
+{
+	echo "error File ${3} cannot be found"
+	exit 1
+}
+fi
+
+knock -v $1 7000 8000 9000
+if [[ -d "${3}" ]] ; then
+{
+  scp -vvvr $3 $2:$4
+}
+else
+{
+  scp -vvv $3 $2:$4
+}
+fi
+
